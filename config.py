1b150aeb79c07220e62a472fb84908a0a67f3ed7
jass132 <140144345+jass132@users.noreply.github.com>
2023-08-12 19:12:23 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,19 +7,19 @@ load_dotenv()
 
 # FOR CODES
 
-API_ID = int(getenv("27288008")) 
-API_HASH = getenv("c5eae746d8e903fd9bf53752a96503b7") 
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5975303993").split()))
-LOGGER = int(getenv("https://t.me/+FK54-SHNv8owNGU1", None)) 
-OWNER = int(getenv("5975303993")) 
-NAME = getenv("Jass")
-OWN_USERNAME= getenv("unknown_bnde")
+API_ID = int(getenv("API_ID")) 
+API_HASH = getenv("API_HASH") 
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "5391883908").split()))
+LOGGER = int(getenv("LOGGER", None)) 
+OWNER = int(getenv("OWNER_ID")) 
+NAME = getenv("ALIVE_NAME")
+OWN_USERNAME= getenv("OWN_USERNAME")
 ALIVE_PIC = getenv("ALIVE_PIC") 
 
 # FOR SPAMBOT
 
-TOKEN1 = getenv("6567386524:AAFLjyqi6mmyWAqd8xc_pVxadL9tCNPEMF4", None) 
-TOKEN2 = getenv("6491700802:AAF9WKLBpA4gCc9sYuVPnLrIFTnBPi-i_d4", None) 
+TOKEN1 = getenv("TOKEN1", None) 
+TOKEN2 = getenv("TOKEN2", None) 
 TOKEN3 = getenv("TOKEN3", None) 
 TOKEN4 = getenv("TOKEN4", None) 
 TOKEN5 = getenv("TOKEN5", None) 
